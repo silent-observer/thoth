e97f446473216db16f1514d7a8984bc9f2f6d49c
silent-observer <maxmi@bk.ru>
2021-12-25 11:16:42 +0300

    Change report page design

--- static/scripts/reported.js
+++ static/scripts/reported.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('.hide-icon').click(function() {
+    $('.hide-btn').click(function() {
         if ($(this).attr('q_id') !== undefined) {
             $.post('/hide', {
                 'q_id': $(this).attr('q_id'),
@@ -10,6 +10,24 @@ $(document).ready(function() {
             })
         } else if ($(this).attr('c_id') !== undefined) {
             $.post('/hide', {
+                'c_id': $(this).attr('c_id')
+            })
+        }
+
+        $(this).closest('.question, .answer, .comment').hide()
+    })
+
+    $('.unreport-btn').click(function() {
+        if ($(this).attr('q_id') !== undefined) {
+            $.post('/unreport', {
+                'q_id': $(this).attr('q_id'),
+            })
+        } else if ($(this).attr('a_id') !== undefined) {
+            $.post('/unreport', {
+                'a_id': $(this).attr('a_id')
+            })
+        } else if ($(this).attr('c_id') !== undefined) {
+            $.post('/unreport', {
                 'c_id': $(this).attr('c_id')
             })
         }
